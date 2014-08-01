78e9db040e3fc41039bbb90f6b93feacde024261
Nate Beaty <nate@clixel.com>
2014-08-01 16:10:01 -0500

    cruft

--- settings.py
+++ settings.py
@@ -37,31 +37,22 @@ SITE_URL = 'lab-zine.com'
 SESSION_COOKIE_AGE = 31104000
 
 # Local time zone for this installation. All choices can be found here:
-# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
 TIME_ZONE = 'Etc/GMT+8'
 
 # Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 2
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-# MEDIA_ROOT = '/var/www/labzine/media/'
-# MEDIA_ROOT = '/home/natebeaty/django/django_projects/labzine/media/'
-# MEDIA_ROOT = '/home/natron46/django_projects/labzine/media/'
-# MEDIA_ROOT = '/home/natebeaty/domains/lab-zine.com/web/public/media/'
 MEDIA_ROOT = '/home/natebeaty/webapps/labzine_static/'
 
 # URL that handles the media served from MEDIA_ROOT.
-# Example: "http://media.lawrence.com"
 MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
@@ -71,7 +62,6 @@ SECRET_KEY = ''
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,19 +72,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.doc.XViewMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'labzine.middleware.threadlocals.ThreadLocals', 
-    # 'djangologging.middleware.LoggingMiddleware',
 )
 
 ROOT_URLCONF = 'labzine.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates".
-    # Always use forward slashes, even on Windows.
-    # '/Users/natebeaty/django/django_templates',
-    # '/Users/natebeaty/django/django_projects/pdxguide/templates',
-    # '/home/natebeaty/django/django_templates',
-    # '/home/natebeaty/django/django_projects/labzine/templates',
-    # '/home/natron46/django_templates',
     '/home/natebeaty/webapps/django/labzine/templates',
 )
 
@@ -119,7 +101,6 @@ INSTALLED_APPS = (
     'labzine.tagsfield',
     'labzine.articles',
     'labzine.blog',
-    # 'django.contrib.comments',
 )
 
 # CACHES = {
